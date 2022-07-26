bde61a0ca95fb14ed8d2908a40654847e6bb19a5
lvjx7 <lvjx7@meicloud.com>
2022-07-26 11:40:23 +0800

    chore: update

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -25,6 +25,7 @@ module.exports = {
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-misused-promises': [ 'warn', { checksVoidReturn: false } ],
+        '@typescript-eslint/require-await': 'off',
       },
     },
   ],
