90a3e2c55964672880ec3b7f6561c0c67167d817
lvjx7 <lvjx7@meicloud.com>
2022-12-01 17:10:00 +0800

    feat: install eslint meanwhile.

--- packages/all/add.js
+++ packages/all/add.js
@@ -1,4 +1,4 @@
-import { execa } from 'execa'
+import { execaSync } from 'execa'
 import path from 'path'
 import fs from 'fs'
 
@@ -16,6 +16,5 @@ pkgInfo.eslintConfig = {
 
 fs.writeFileSync(pkgPath, JSON.stringify(pkgInfo, null, 2) + '\n')
 
-
 // Install
-execa('ni', [ '@lvjiaxuan/eslint-config', '-D' ], { stdio: 'inherit', cwd })
+execaSync('ni', [ '@lvjiaxuan/eslint-config', 'eslint', '-D' ], { stdio: 'inherit', cwd })
