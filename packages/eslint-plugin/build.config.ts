f615da3429125d0b70acdfa889075cc17dcf1300
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2023-03-01 17:17:40 +0800

    fix: export compat problem from tsup

---
+++ packages/eslint-plugin/build.config.ts
@@ -0,0 +1,14 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: [
+    './src/index',
+    './add',
+  ],
+  clean: true,
+  // declaration: true,
+  failOnWarn: false,
+  'rollup': {
+    'emitCJS': true
+  }
+})
