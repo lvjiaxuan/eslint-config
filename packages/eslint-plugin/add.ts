77d90a876b6543fe0cbc95888e96a0a6bbf1b567
unknown <11309921+lvjiaxuan@users.noreply.github.com>
2023-03-05 22:11:56 +0800

    wip

--- packages/eslint-plugin/add.ts
+++ packages/eslint-plugin/add.ts
@@ -1,10 +1,8 @@
 import { execaSync } from 'execa'
 import path from 'node:path'
 import fs from 'node:fs'
-import minimist from 'minimist'
 
-const args = minimist(process.argv.slice(2))
-const flat = !!args.flat
+const flat = process.argv.slice(2).includes('--flat')
 
 try {
 
@@ -13,30 +11,35 @@ try {
   // Install
   execaSync('ni', [ '@lvjiaxuan/eslint-plugin', 'eslint', '-D', '--workspace-root=true' ], { stdio: 'inherit', cwd })
 
-  // Setup
-  const pkgPath = path.resolve(cwd, 'package.json')
-  const pkgInfo = JSON.parse(fs.readFileSync(pkgPath, 'utf-8')) as {
-    scripts?: { [x: string]: string }
-    eslintConfig?: {
-      extends?: string | string[]
-    }
-  }
-  pkgInfo.eslintConfig = pkgInfo.eslintConfig ?? {}
-
-  if (typeof pkgInfo.eslintConfig.extends === 'string') {
-    pkgInfo.eslintConfig.extends = [ pkgInfo.eslintConfig.extends, 'plugin:@lvjiaxuan/recommended' ]
-  } else if (Array.isArray(pkgInfo.eslintConfig.extends)) {
-    pkgInfo.eslintConfig.extends = [ ...pkgInfo.eslintConfig.extends, 'plugin:@lvjiaxuan/recommended' ]
+  if(flat) {
+    fs.writeFileSync(path.resolve(cwd, 'eslint.config.js'), `import lvjiaxuan from '@lvjiaxuan/eslint-plugin/flat'\n\nexport default lvjiaxuan.configs.recommended`, { encoding: 'utf-8' })
   } else {
-    pkgInfo.eslintConfig = { extends: 'plugin:@lvjiaxuan/recommended' }
+    // Setup
+    const pkgPath = path.resolve(cwd, 'package.json')
+    const pkgInfo = JSON.parse(fs.readFileSync(pkgPath, 'utf-8')) as {
+      scripts?: { [x: string]: string }
+      eslintConfig?: {
+        extends?: string | string[]
+      }
+    }
+    pkgInfo.eslintConfig = pkgInfo.eslintConfig ?? {}
+  
+    if (typeof pkgInfo.eslintConfig.extends === 'string') {
+      pkgInfo.eslintConfig.extends = [ pkgInfo.eslintConfig.extends, 'plugin:@lvjiaxuan/recommended' ]
+    } else if (Array.isArray(pkgInfo.eslintConfig.extends)) {
+      pkgInfo.eslintConfig.extends = [ ...pkgInfo.eslintConfig.extends, 'plugin:@lvjiaxuan/recommended' ]
+    } else {
+      pkgInfo.eslintConfig = { extends: 'plugin:@lvjiaxuan/recommended' }
+    }
+  
+    pkgInfo.scripts = pkgInfo.scripts ?? {}
+    !pkgInfo.scripts.lint && (
+      pkgInfo.scripts.lint = 'eslint .'
+    )
+  
+    fs.writeFileSync(pkgPath, JSON.stringify(pkgInfo, null, 2) + '\n')
   }
 
-  pkgInfo.scripts = pkgInfo.scripts ?? {}
-  !pkgInfo.scripts.lint && (
-    pkgInfo.scripts.lint = 'eslint .'
-  )
-
-  fs.writeFileSync(pkgPath, JSON.stringify(pkgInfo, null, 2) + '\n')
 
   process.exit(0)
 } catch (error) {
