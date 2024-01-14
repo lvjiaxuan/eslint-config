faac1908123362cd0a1d2d4577ec43db973d2f20
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2024-01-14 23:27:47 +0800

    feat: detect tsconfig if ts enables.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { join } from 'node:path'
 import process from 'node:process'
-import antfu, { GLOB_SRC } from '@antfu/eslint-config'
+import antfu, { typescript } from '@antfu/eslint-config'
+import type { FlatConfigItem, OptionsTypeScriptWithTypes } from '@antfu/eslint-config'
 import pluginLv from '@lvjiaxuan/eslint-plugin'
-import type { FlatConfigItem } from '@antfu/eslint-config'
 import { pathExists } from 'fs-extra'
 
 const pluginItem: FlatConfigItem = {
-  files: [GLOB_SRC],
+  files: ['*'],
   name: '@lvjiaxuan:plugin',
   plugins: {
     '@lvjiaxuan': pluginLv,
@@ -17,16 +17,43 @@ const pluginItem: FlatConfigItem = {
 }
 
 async function detectTsconfigPath() {
-  const defaultFile = 'tsconfig.json'
+  const defaultFile = 'tsconfig.json' as const
   if (await pathExists(join(process.cwd(), defaultFile)))
     return defaultFile
 }
 
-const lv: typeof antfu = (...args) => {
-  return antfu(
+const lv: typeof antfu = async (...args) => {
+  const merged = await antfu(
     ...args,
     pluginItem,
-  )
+  ) as FlatConfigItem[]
+
+  if (merged.find(item => item.name === 'antfu:typescript:setup')) {
+    // Means ts is setup
+    let tsOptions = args[0]?.typescript
+
+    // Overwrite with tsconfigPath
+    if (typeof tsOptions === 'object') {
+      (tsOptions as OptionsTypeScriptWithTypes).tsconfigPath ??= await detectTsconfigPath()
+    }
+    else {
+      // @ts-expect-errord typescript = true means {}
+      ;(tsOptions as OptionsTypeScriptWithTypes) = { tsconfigPath: await detectTsconfigPath() }
+    }
+
+    // New ts options with `tsconfigPath`.
+    const tsItems = await typescript(tsOptions as OptionsTypeScriptWithTypes)
+
+    const tsRulesItemName = 'antfu:typescript:rules'
+    const tsRulesItemIdx = merged.findIndex(item => item.name === tsRulesItemName)
+    merged.splice(tsRulesItemIdx, 1, tsItems.find(item => item.name === tsRulesItemName)!)
+
+    const tsTypeAwareItemName = 'antfu:typescript:rules-type-aware'
+    const tsTypeAwareItemIdx = merged.findIndex(item => item.name === tsTypeAwareItemName)
+    merged.splice(tsTypeAwareItemIdx, 1, tsItems.find(item => item.name === tsTypeAwareItemName)!)
+  }
+
+  return merged
 }
 
 export default lv
