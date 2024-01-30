2c2260ea2e7940fc134292806d5bd03bff837189
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2024-01-30 17:38:32 +0800

    chore: lint no `category-rules.json` .

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,9 @@ import lv from './src'
 
 export default lv({
   oxlint: true,
+  ignores: ['packages/eslint-plugin-oxlint/category-rules.json'],
+}, {
   rules: {
-    'no-console': 'off'
-  }
+    'no-console': 'off',
+  },
 })
