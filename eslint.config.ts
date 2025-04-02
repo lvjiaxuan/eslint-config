ebfb3e59988b4509fc94f217b6a941dc820acc42
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2025-04-02 16:57:23 +0800

    chore: update dev

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,6 @@
-import lv from './packages/eslint-config/src/lv'
-// import lv from '@lvjiaxuan/eslint-config'
+// https://github.com/egoist/bundle-require/issues/40
+// import lv from './packages/eslint-config/src/lv'
+
+import lv from '@lvjiaxuan/eslint-config'
 
 export default lv({ oxlint: { deny: 'perf' } })
