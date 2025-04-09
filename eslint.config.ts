e240171ed6107d36956534fc4f24e017a442aa8d
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2025-04-09 14:54:49 +0800

    feat: add respectjs4ts rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,3 @@
-// https://github.com/egoist/bundle-require/issues/40
-// import lv from './packages/eslint-config/src/lv'
+import lv from './packages/eslint-config/src/lv'
 
-import lv from '@lvjiaxuan/eslint-config'
-
-export default lv({ oxlint: { deny: 'perf' } })
+export default lv({ oxlint: true })
