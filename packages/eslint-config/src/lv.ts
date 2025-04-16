b5a2e278f7974eefb1254bbea1eed71c820d38d0
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2025-04-16 17:38:41 +0800

    fix: `lv()` params type

--- packages/eslint-config/src/lv.ts
+++ packages/eslint-config/src/lv.ts
@@ -1,11 +1,11 @@
-import type { OverrideAntfuParams } from '.'
+import type { LvParams } from '.'
 import antfu from '@antfu/eslint-config'
 import { lvPlugin } from '@lvjiaxuan/eslint-plugin'
 import { oxlint, respectJsRuleOptions } from '.'
 
 export default lv
 
-export async function lv(...args: OverrideAntfuParams) {
+export async function lv(...args: LvParams) {
   const [afOptionsConfig] = args
   const pipeline = antfu(...args)
 
